ecfa0a2162cd07db0539209ff3e89a412baca31d
Jan Bureš <jan.bures@live.com>
2023-08-11 14:34:35 +0200

    Printing "Hello, world!"

--- src/main.asm
+++ src/main.asm
@@ -4,13 +4,64 @@ org 0x7C00
 bits 16
 
 
+%define ENDL 0x0D, 0x0A ; Newline character
+
+
+start:
+    jmp main
+
+
+;
+; Print a string to the screen
+; Parameters:
+;   ds:si - Pointer to the string to print
+;
+puts:
+    ; Save registers that we're going to modify
+    push si
+    push ax
+
+.loop:
+    lodsb ; Load the next char from ds:si into al, and increment si
+    or al, al ; Set the zero flag if character in al is null
+    jz .done ; If the zero flag is set, we're done
+
+    mov ah, 0x0E ; BIOS teletype function
+    mov bh, 0x00 ; Page number
+    int 0x10 ; Call BIOS interrupt 0x10 (video services)
+
+    jmp .loop ; Repeat until the string is exhausted
+
+.done:
+    ; Restore registers
+    pop ax
+    pop si
+    ret
+
+
 main:
+    ; Set up data segments
+    mov ax, 0 ; Can't write to ds/es directly because it's a 16-bit register
+    mov ds, ax
+    mov es, ax
+
+    ; Set up stack
+    mov ss, ax
+    mov sp, 0x7C00 ; Stack grows downwards, so we set it to the start of our OS
+
+    ; Print a message
+    mov si, msg_hello
+    call puts
+
     ; Halt the processor
     hlt
 
 .halt:
     ; If the processor starts executing code again, trap it in an infinite loop
     jmp .halt
+
+
+msg_hello: db 'Hello, world!', ENDL, 0
 
 
 ; $ = memory offset of current line in bytes, $$ = memory offset of current segment
